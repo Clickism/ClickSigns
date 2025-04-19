cf5f8116513342d683b58ecd54d62798ea54c370
Clickism <yagizaktasx@gmail.com>
2025-04-19 14:39:05 +0200

    add version 1.21.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ stonecutter {
 	kotlinController = true
 	centralScript = "build.gradle.kts"
 	create(rootProject) {
-		versions("1.20.1", "1.21.1", "1.21.4")
-		vcsVersion = "1.21.4"
+		versions("1.20.1", "1.21.1", "1.21.4", "1.21.5")
+		vcsVersion = "1.21.5"
 	}
 }
