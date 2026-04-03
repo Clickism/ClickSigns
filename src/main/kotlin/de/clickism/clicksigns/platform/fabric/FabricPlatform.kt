cba2b0048027e29c6d79bcd14f043db18b1abe0d
Clickism <yagizaktasx@gmail.com>
2026-04-03 11:25:24 +0200

    change versions

--- src/main/kotlin/de/clickism/clicksigns/platform/fabric/FabricPlatform.kt
+++ src/main/kotlin/de/clickism/clicksigns/platform/fabric/FabricPlatform.kt
@@ -7,6 +7,7 @@ import net.minecraft.core.Registry
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceKey
+import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockBehaviour
