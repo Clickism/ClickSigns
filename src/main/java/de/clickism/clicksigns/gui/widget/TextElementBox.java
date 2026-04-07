596f9e9da6dcbf4dcbd4355db3772eecd86e3423
Clickism <yagizaktasx@gmail.com>
2026-04-07 17:02:25 +0200

    fix alignment on text element box

--- src/main/java/de/clickism/clicksigns/gui/widget/TextElementBox.java
+++ src/main/java/de/clickism/clicksigns/gui/widget/TextElementBox.java
@@ -2,13 +2,8 @@ package de.clickism.clicksigns.gui.widget;
 
 import de.clickism.clicksigns.gui.GuiUtils;
 import de.clickism.clicksigns.sign.element.TextElement;
-import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.EditBox;
-import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.network.chat.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static de.clickism.clicksigns.gui.widget.TextureWidget.TEXTURE_RENDER_SCALE;
 
@@ -18,18 +13,25 @@ import static de.clickism.clicksigns.gui.widget.TextureWidget.TEXTURE_RENDER_SCA
 public class TextElementBox extends EditBox {
     private static final float TEXT_BOX_HEIGHT = 4f;
 
-    private final TextElement textElement;
+    private final TextElement text;
 
     /**
      * Creates a new text element box.
      */
-    public TextElementBox(TextElement textElement, int rootX, int rootY) {
-        super(GuiUtils.font(), rootX, rootY, 100, (int) (TEXT_BOX_HEIGHT * TEXTURE_RENDER_SCALE * textElement.scale()), Component.empty());
-        this.textElement = textElement;
+    public TextElementBox(TextElement text, int rootX, int rootY) {
+        super(GuiUtils.font(), rootX, rootY, 100, (int) (TEXT_BOX_HEIGHT * TEXTURE_RENDER_SCALE * text.scale()), Component.empty());
+        this.text = text;
         // Calculate position
-        int xOffset = (int) (textElement.localX() * TEXTURE_RENDER_SCALE);
-        int yOffset = (int) (textElement.localY() * TEXTURE_RENDER_SCALE);
-        this.setX(rootX + xOffset);
-        this.setY(rootY + yOffset - this.height);
+        int offsetX = (int) (text.localX() * TEXTURE_RENDER_SCALE);
+        int offsetY = (int) (text.localY() * TEXTURE_RENDER_SCALE);
+        // By default, should be centered
+        offsetX -= this.width / 2;
+        offsetY -= this.height / 2;
+        // Apply alignment offset
+        offsetX += (int) (text.alignment().offset().x * width);
+        offsetY += (int) (text.alignment().offset().y * height);
+        // Set position
+        this.setX(rootX + offsetX);
+        this.setY(rootY + offsetY - this.height);
     }
 }
