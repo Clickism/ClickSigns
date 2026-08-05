315e05b001c07b2a7f1e2e6cde16891fcc734c72
Prpr <napstaa967alt@gmail.com>
2026-08-04 18:35:12 -0600

    templates

--- src/main/java/de/clickism/clicksigns/sign/texture/source/TextureSource.java
+++ src/main/java/de/clickism/clicksigns/sign/texture/source/TextureSource.java
@@ -1,6 +1,7 @@
 package de.clickism.clicksigns.sign.texture.source;
 
 import de.clickism.clicksigns.ClickSigns;
+import de.clickism.clicksigns.registry.SignRegistries;
 import de.clickism.clicksigns.sign.ColorResolver;
 import de.clickism.clicksigns.sign.texture.Texture;
 import de.clickism.clicksigns.util.PixelSized;
@@ -8,6 +9,7 @@ import de.clickism.clicksigns.util.nbt.NbtReader;
 import de.clickism.clicksigns.util.nbt.NbtWriter;
 import de.clickism.clicksigns.util.nbt.TypeKeyed;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 //? if >= 1.21.1
 import net.minecraft.network.codec.StreamCodec;
 
@@ -60,6 +62,42 @@ public sealed interface TextureSource extends TypeKeyed permits StaticTextureSou
     default TextureSource resizeToFit(PixelSized sized) {
         return resize(sized.width(), sized.height());
     }
+
+    /**
+     * Parses the given texture identifier as tileset or static texture.
+     * <p>
+     * Only support static or tiled textures for templates!
+     *
+     * @param location the texture identifier to parse
+     * @param width    the width of the texture in pixels
+     * @param height   the height of the texture in pixels
+     * @return texture source
+     */
+    static TextureSource parse(ResourceLocation location, int width, int height) {
+        if (SignRegistries.TILE_SETS.has(location)) {
+            return new TiledTextureSource(location, width, height);
+        }
+        return new StaticTextureSource(location);
+    }
+
+    /**
+     * Gets the texture location of the given texture source.
+     * <p>
+     * Only support static or tiled textures for templates!
+     *
+     * @param source the texture source to get the location of
+     * @return the texture location of the source
+     */
+    static ResourceLocation textureLocationOf(TextureSource source) {
+        if (source instanceof StaticTextureSource staticSource) {
+            return staticSource.location();
+        }
+        if (source instanceof TiledTextureSource tiledSource) {
+            return tiledSource.tileSetId();
+        }
+        throw new IllegalArgumentException("Unsupported texture source type: " + source.getClass().getName());
+    }
+
 
     //? if >= 1.21.1 {
     StreamCodec<FriendlyByteBuf, TextureSource> PACKET = StreamCodec.of(
