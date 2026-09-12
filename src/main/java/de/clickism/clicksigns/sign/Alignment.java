3161f62399d869df03ff27d1c6888ba88200b662
Clickism <yagizaktasx@gmail.com>
2026-09-12 12:02:28 +0200

    refactor alignments

--- src/main/java/de/clickism/clicksigns/sign/Alignment.java
+++ src/main/java/de/clickism/clicksigns/sign/Alignment.java
@@ -2,6 +2,8 @@ package de.clickism.clicksigns.sign;
 
 import org.joml.Vector2f;
 
+import java.util.List;
+
 /**
  * Alignment for rendering elements
  */
@@ -20,6 +22,7 @@ public enum Alignment {
     public static final Alignment TEXT_LEFT = TOP_LEFT;
     public static final Alignment TEXT_CENTER = TOP_CENTER;
     public static final Alignment TEXT_RIGHT = TOP_RIGHT;
+
     private final Vector2f offset;
 
     Alignment(float offsetX, float offsetY) {
@@ -34,5 +37,42 @@ public enum Alignment {
      */
     public Vector2f offset() {
         return new Vector2f(offset);
+    }
+
+    /**
+     * Returns the opposite alignment.
+     *
+     * @return the opposite alignment
+     */
+    public Alignment opposite() {
+        return switch (this) {
+            case TOP_LEFT -> BOTTOM_RIGHT;
+            case TOP_CENTER -> BOTTOM_CENTER;
+            case TOP_RIGHT -> BOTTOM_LEFT;
+            case CENTER_LEFT -> CENTER_RIGHT;
+            case CENTER -> CENTER;
+            case CENTER_RIGHT -> CENTER_LEFT;
+            case BOTTOM_LEFT -> TOP_RIGHT;
+            case BOTTOM_CENTER -> TOP_CENTER;
+            case BOTTOM_RIGHT -> TOP_LEFT;
+        };
+    }
+
+    /**
+     * Returns a list of all alignments.
+     *
+     * @return list of all alignments
+     */
+    public static List<Alignment> all() {
+        return List.of(values());
+    }
+
+    /**
+     * Returns a list of all text alignments.
+     *
+     * @return list of all text alignments
+     */
+    public static List<Alignment> textAlignments() {
+        return List.of(TEXT_LEFT, TEXT_CENTER, TEXT_RIGHT);
     }
 }
