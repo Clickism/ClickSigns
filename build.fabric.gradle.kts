87e0706a656f844bd44bf599afc2d57ea0a0a035
Clickism <yagizaktasx@gmail.com>
2026-09-20 04:30:17 +0200

    update buildscripts

--- build.fabric.gradle.kts
+++ build.fabric.gradle.kts
@@ -116,6 +116,8 @@ publishMods {
         minecraftVersions.addAll(mcVersions)
     }
     github {
+        displayName.set("${property("mod.version")} (Forge)")
+        type.set(STABLE)
         accessToken.set(System.getenv("GITHUB_TOKEN"))
         repository.set("Clickism/ClickSigns")
         commitish = "master"
